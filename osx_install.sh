2991b3b49237b1df6820dc5ee9725e6d704a2f7f
Steve Dignam <steve@dignam.xyz>
2015-08-16 13:51:52 -0400

    Added install for xcode cli tools

--- osx_install.sh
+++ osx_install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Install Xcode Command Line Tools
+xcode-select --install
+
 # Install Homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
